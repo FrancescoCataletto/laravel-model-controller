a7588fa2f361a75ed9cd55f5f6b72d329665f3ce
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-24 22:01:58 +0200

    added some style to the cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-@foreach ($movies as $movie)
-<div>
-    <ul>
-        <li>{{$movie->title}}</li>
-        <li>{{$movie->original_title}}</li>
-        <li>{{$movie->nationality}}</li>
-        <li>{{$movie->date}}</li>
-        <li>{{$movie->vote}}</li>
-    </ul>
+<div id="main">
+    @foreach ($movies as $movie)
+    <div id="single-card">
+        <ul>
+            <li><span>Titolo:</span> {{$movie->title}}</li>
+            <li><span>Titolo originale:</span> {{$movie->original_title}}</li>
+            <li><span>Paese:</span> {{$movie->nationality}}</li>
+            <li><span>Anno di produzione:</span> {{$movie->date}}</li>
+            <li><span>Voto:</span> {{$movie->vote}}</li>
+        </ul>
+    </div>
+    @endforeach
 </div>
-@endforeach
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,10 @@
-<h1>HEADER</h1>
+<header>
+    <div id="menu">
+        <ul>
+            <li><a href="">Film</a></li>
+            <li><a href="">Ordine alfabetico</a></li>
+            <li><a href="">Pre muro di berlino</a></li>
+            <li><a href="">Post muro di berlino</a></li>
+        </ul>
+    </div>
+</header>
